da0c58853aca0d72c5fe8c4c0f7f56b6b6218341
johnny-keker <giomail.iv@gmail.com>
2019-09-16 12:48:12 +0300

    text-entropy-react: init

---
+++ text-entropy-react/src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { async } from 'q';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            { this.state.text }
+        </p>
+          <div id="json-file-loader">
+            <input type="file" ref="file" />
+          </div>
+          <button onClick={this.openFile}>Load File</button>
+        </header>
+      </div>
+    );
+  }
+
+  openFile = async () => {
+    const rawFile = await readFileAsync(this.refs.file.files[0]);
+    const text = arrayBufferToString(rawFile);
+    this.setState({ text });
+  }
+}
+
+function readFileAsync(file) {
+  return new Promise((resolve, reject) => {
+    let reader = new FileReader();
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = reject;
+    reader.readAsArrayBuffer(file);
+  });
+}
+
+function arrayBufferToString(arrayBuffer) {
+  return new TextDecoder('utf-8').decode(arrayBuffer);
+}
+
+export default App;
