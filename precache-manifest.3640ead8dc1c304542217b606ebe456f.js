0c3959319ad0408df51483b0d3e4b0b0408a792e
johnny-keker <giomail.iv@gmail.com>
2019-09-23 09:59:55 +0300

    Updates

---
+++ precache-manifest.3640ead8dc1c304542217b606ebe456f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf9cac72a6233e0705e7cfedb0925f11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa2cc18c51ff3e28f3a0",
+    "url": "/static/css/main.7efeb664.chunk.css"
+  },
+  {
+    "revision": "25a0c0235c0e7765b988",
+    "url": "/static/js/2.50a823ef.chunk.js"
+  },
+  {
+    "revision": "fa2cc18c51ff3e28f3a0",
+    "url": "/static/js/main.470203f4.chunk.js"
+  },
+  {
+    "revision": "a85f6a06741ca1414ed1",
+    "url": "/static/js/runtime~main.6a07da3a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
