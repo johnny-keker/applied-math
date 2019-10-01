19e29c2553e1517631cc46079e422f84af561e97
johnny-keker <giomail.iv@gmail.com>
2019-10-01 23:24:22 +0300

    [lab2] huffman first step

--- lab2/lib/lab2.ex
+++ lab2/lib/lab2.ex
@@ -17,8 +17,10 @@ defmodule Lab2 do
               |> Enum.uniq()
               |> Enum.map(fn x -> {x, (length(String.split(contents, x)) - 1) / String.length(contents)} end)
     
-    res = shennon_fano("", probs, [])
-    IO.inspect(res)
+    shennon_res = shennon_fano("", probs, [])
+    IO.inspect(shennon_res)
+    IO.inspect(huffman(probs))
+
   end
 
   def shennon_fano(key, data, acc) do
@@ -48,6 +50,22 @@ defmodule Lab2 do
       else
         shennon_fano_separation(rest, left, [element | right])
       end
+    end
+  end
+
+  def huffman(data) do
+    if length(data) == 2 do
+      [{char1, prob1}, {char2, prob2}] = data
+      [{"0", char1, prob1}, {"1", char2, prob2}]
+    else
+      [min1, min2] = data
+                     |> Enum.sort_by(fn {char, prob} -> prob end)
+                     |> Enum.take(2)
+
+      {min1_char, min1_prob} = min1
+      {min2_char, min2_prob} = min2
+
+      huffman([{"#{min1_char}#{min2_char}", min1_prob + min2_prob} | Enum.filter(data, fn {char, prob} -> char != min1_char && char != min2_char end)])
     end
   end
 end
